608984ca085798bc49960433ffd61f0eebd31210
bratashenko.p <bratashenko.p@gmail.com>
2022-05-26 15:00:02 +0300

    Создание методов: чтения из файла и записи в коллекцию и запись в файл с коллекции.

---
+++ src/ua/com/javarash/bratashenko/сaesar/Cipher.java
@@ -0,0 +1,44 @@
+package ua.com.javarash.bratashenko.сaesar;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cipher {
+
+
+    public final List<Character> ALPHABET = Arrays.asList('а', 'б', 'в',
+            'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
+    public final int ALPHABET_SIZE = ALPHABET.size();
+
+
+    public ArrayList<Character> fileToCollection(FileReader reader,
+                                                 ArrayList<Character> list) throws IOException {
+        int charaster;
+
+        while ((charaster = reader.read()) != -1) {
+            list.add((char) charaster);
+        }
+        return list;
+    }
+
+    public ArrayList<Character> shift(ArrayList<Character> listI, int key, ArrayList<Character> listO) {
+        for (int i = 0; i < listI.size(); i++) {
+            listO.add(ALPHABET.get(ALPHABET.indexOf(listI.get(i)) + key % ALPHABET_SIZE));
+        }
+        return listO;
+    }
+
+    public void collectionToFile(ArrayList<Character> list, FileWriter writer) throws IOException {
+        for (int i = 0; i < list.size(); i++) {
+            writer.write(list.get(i));
+        }
+
+    }
+
+}
--- src/ua/com/javarash/bratashenko/сaesar/Main.java
+++ src/ua/com/javarash/bratashenko/сaesar/Main.java
@@ -1,39 +1,35 @@
 package ua.com.javarash.bratashenko.сaesar;
 
+import javax.crypto.Cipher;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-    private static final List<Character> ALPHABET = Arrays.asList('а', 'б', 'в',
-            'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
-            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
-            ':', '!', '?', ' ');
-    public static final int ALPHABET_SIZE = ALPHABET.size();
+    public static void main(String[] args) {
 
-    public static ArrayList<Character> shift(ArrayList<Character> list, int key) {
+        ArrayList<Character> charListIn = new ArrayList<>();
         ArrayList<Character> charListOut = new ArrayList<>();
 
-        for (int i = 0; i < list.size(); i++) {
-            charListOut.add(ALPHABET.get(ALPHABET.indexOf(list.get(i)) + key  % ALPHABET_SIZE));
+
+        try (FileReader reader = new FileReader("in.txt");
+             FileWriter writer = new FileWriter("out.txt");) {
+        } catch (IOException e) {
+            System.err.println("Некорректное имя файла или путь" + e);
         }
-        return charListOut;
-    }
 
 
-    public static void main(String[] args) {
-        ArrayList<Character> charListIn = new ArrayList<>();
 
 
-        charListIn.add('a');
-        charListIn.add('b');
-        charListIn.add('b');
-        charListIn.add('a');
 
 
-        System.out.println(shift(charListIn, 3));
 
 
     }
 }
+
