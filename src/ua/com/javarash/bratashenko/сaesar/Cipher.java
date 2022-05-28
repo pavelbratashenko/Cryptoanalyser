49f96e849b877e976685330cc602fcf5d28b9290
bratashenko.p <bratashenko.p@gmail.com>
2022-05-28 19:07:13 +0300

    Создание метода с дешифровкой. Разбитие по логике: работа с файлами, шифровка/дешифровка.

--- src/ua/com/javarash/bratashenko/сaesar/Cipher.java
+++ src/ua/com/javarash/bratashenko/сaesar/Cipher.java
@@ -1,8 +1,5 @@
 package ua.com.javarash.bratashenko.сaesar;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,15 +10,7 @@ public class Cipher {
             'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
             ':', '!', '?', ' ');
     public final int ALPHABET_SIZE = ALPHABET.size();
-    public ArrayList<Character> fileToCollection(FileReader reader,
-                                                 ArrayList<Character> list) throws IOException {
-        int charaster;
 
-        while ((charaster = reader.read()) != -1) {
-            list.add((char)charaster);
-        }
-        return list;
-    }
     public ArrayList<Character> shift(ArrayList<Character> listI, int key, ArrayList<Character> listO) {
         for (int i = 0; i < listI.size(); i++) {
             listO.add(ALPHABET.get(ALPHABET.indexOf(listI.get(i)) + key % ALPHABET_SIZE));
@@ -29,9 +18,18 @@ public class Cipher {
         return listO;
     }
 
-    public void collectionToFile(ArrayList<Character> list, FileWriter writer) throws IOException {
-        for (int i = 0; i < list.size(); i++) {
-            writer.write(list.get(i));
+    public ArrayList<Character> shiftBack(ArrayList<Character> listI, int key,
+                                          ArrayList<Character> listO) {
+
+        for (int i = 0; i < listI.size(); i++) {
+            if  ((ALPHABET.get(ALPHABET.indexOf(listI.get(i)) - key) < 0)) {
+                int temp = ALPHABET.size() - (ALPHABET.get(ALPHABET.indexOf(listI.get(i)) - key));
+                listO.add(ALPHABET.get(temp));
+            }else {
+                listO.add(ALPHABET.get(ALPHABET.indexOf(listI.get(i)) - key));
+            }
         }
+        return listO;
     }
+
 }
--- src/ua/com/javarash/bratashenko/сaesar/Main.java
+++ src/ua/com/javarash/bratashenko/сaesar/Main.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 
 public class Main {
-
     public static void main(String[] args) {
 
         ArrayList<Character> charListIn = new ArrayList<>();
@@ -16,18 +15,18 @@ public class Main {
 
         Cipher cipher = new Cipher();
 
-        try {
-            cipher.fileToCollection(new FileReader("in.txt"), charListIn);
-        } catch (IOException e) {
-            System.err.println("Некорректное имя файла или путь" + e);
-        }
-        cipher.shift(charListIn, 3, charListOut);
-
-        try {
-            cipher.collectionToFile(charListOut, new FileWriter("out.txt"));
-        } catch (IOException e) {
-            System.err.println("Некорректное имя файла или путь" + e);
-        }
+//        try {
+//            cipher.fileToCollection(new FileReader("C:\\Users\\brata\\Desktop\\qq.txt"), charListIn);
+//        } catch (IOException e) {
+//            System.err.println("Некорректное имя файла или путь" + e);
+//        }
+//        cipher.shift(charListIn, 3, charListOut);
+//
+//        try {
+//            cipher.collectionToFile(charListOut, new FileWriter("qq1.txt"));
+//        } catch (IOException e) {
+//            System.err.println("Некорректное имя файла или путь" + e);
+//        }
     }
 }
 
